5469ac3a2829896111f4ba5310c3c81d641c5c01
Brorkus <filip.g.danielsen@gmail.com>
2025-10-22 16:45:32 +0200

    start av oppgave

---
+++ obligoppgave/alle-klasser-og-studenter.php
@@ -0,0 +1,26 @@
+<?php
+include("dpconnect.php");
+
+$sqlSetning= "select * from klasse;";
+
+$sqlResultat=mysqli_query($db,$sqlSetning) or die ("Kunne ikke hente data fra database");
+
+$antallRader=mysqli_num_rows($sqlResultat); /*antall rader beregnet*/ 
+
+ print ("<h3>Registrerte klasser</h3>");
+  print ("<table border=1>");   /*tabell start */
+  print ("<tr><th align=left>klassekode</th> 
+          <th align=left>klassenavn</th></tr>
+          <th align=left>studiumkode</th></tr>"); 
+  
+  for ($r=1;$r<=$antallRader;$r++)
+  {
+    $rad=mysqli_fech_array($sqlResultat);
+    $klasseKode=$rad["klassekode"];
+    $klasseNavn=$rad["klassenavn"];
+    $studiumKode=$rad["studiumkode"];
+
+    print("<tr> <td> $klasseKode </td> <td> $klasseNavn </td> <td> $studiumKode </td> </tr>");
+  }
+  print("</table>"); /*tabell slutt */
+  ?>
---
+++ obligoppgave/registrer-klasse.php
@@ -0,0 +1,43 @@
+<?php
+<h3>Registrer klasse </h3>
+
+<form method="post" action="" id="registrerKlasse" name="registrerKlasse">
+  Klassekode <input type="text" id="klasseKode" name="klasseKode" required /> <br/>
+  Klassenavn <input type="text" id="klasseNavn" name="klasseNavn" required /> <br/>
+  Studiumkode <input type="text" id="studiumKode" name="studiumKode" required /> <br/>
+  <input type="submit" value="Registrer klasse" id="registrerKlasseKnapp" name="registrerKlasseKnapp" /> 
+  <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
+</form>
+
+ if (isset($_POST ["registrerKlasseKnapp"]))
+    {
+      $klasseKode=$_POST ["klasseKode"];
+      $klasseNavn=$_POST ["klasseNavn"];
+      $studiumKode=$_POST ["studiumKode"];
+
+      if (!$klasseKode || !$klasseNavn || !$studiumKode)
+        {
+          print ("B&aring;de klassekode, klassenavn og studiumkode m&aring; fylles ut");
+        }
+      else
+        {
+          include("dbconnect.php");  /* tilkobling til database */
+
+          $sqlSetning="SELECT * FROM klasse WHERE klassekode='$klasseKode';";
+          $sqlResultat=mysqli_query($db,$sqlSetning) or die ("Kunne ikke hente data fra database");
+          $antallRader=mysqli_num_rows($sqlResultat); 
+
+          if ($antallRader!=0)  /* Klasse er registrert fra før */
+            {
+              print ("Klasse er allerede registrert");
+            }
+          else
+            {
+              $sqlSetning="INSERT INTO klasse VALUES('$klasseKode','$klassenavn','$studiumKode);";
+              mysqli_query($db,$sqlSetning) or die ("Kunne ikke registrere data i database");
+                /* SQL-setning sendt til database */
+
+              print ("Denne klassen er blitt registrert: $klasseKode $klasseNavn $studiumKode"); 
+            }
+        }
+    }
