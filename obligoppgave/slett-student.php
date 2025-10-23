bee40d079ae07a0635b2930225470edfe47dc700
Brorkus <filip.g.danielsen@gmail.com>
2025-10-23 16:12:13 +0200

    Update slett-student.php

--- obligoppgave/slett-student.php
+++ obligoppgave/slett-student.php
@@ -1,12 +1,14 @@
 <?php
 ?>
 <h3>Slett student<h3>
+<form method="post" action="" id="slettStudentSkjema" name="slettStudentSkjema" onSubmit="return bekreft()">
 Slett student <select name="slettstudent" id="slettstudent">
     <option value="">velg student</option>
     <?php include("funksjoner.php"); listeboksBrukernavn(); ?> 
   </select>  <br/>
   <input type="submit" value="Slett student" id="slettStudentKnapp" name="slettStudentKnapp" /> 
   <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
+</form>
 <?php
  if (isset($_POST ["slettStudentKnapp"]))
     {	
