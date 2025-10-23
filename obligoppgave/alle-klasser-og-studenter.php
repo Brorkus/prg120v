0bf489f5a9f5aa0349f1bf7442b95835de154725
Brorkus <filip.g.danielsen@gmail.com>
2025-10-23 15:00:08 +0200

    Update alle-klasser-og-studenter.php

--- obligoppgave/alle-klasser-og-studenter.php
+++ obligoppgave/alle-klasser-og-studenter.php
@@ -23,4 +23,30 @@ $antallRader=mysqli_num_rows($sqlResultat); /*antall rader beregnet*/
     print("<tr> <td> $klassekode </td> <td> $klassenavn </td> <td> $studiumkode </td> </tr>");
   }
   print("</table>"); /*tabell slutt */
+
+
+  $sqlSetning= "select * from student;";
+
+$sqlResultat=mysqli_query($db,$sqlSetning) or die ("Kunne ikke hente data fra database");
+
+$antallRader=mysqli_num_rows($sqlResultat); /*antall rader beregnet*/ 
+
+ print ("<h3>Registrerte studenter</h3>");
+  print ("<table border=1>");   /*tabell start */
+  print ("<tr><th align=left>brukernavn</th> 
+          <th align=left>fornavn</th>
+          <th align=left>etternavn</th>
+          <th align=left>klassekode</th></tr>"); 
+  
+  for ($r=1;$r<=$antallRader;$r++)
+  {
+    $rad=mysqli_fetch_array($sqlResultat);
+    $brukernavn=$rad["brukernavn"];
+    $fornavn=$rad["fornavn"];
+    $etternavn=$rad["etternavn"];
+    $klassekode=$rad["klassekode"];
+
+    print("<tr> <td> $brukernavn </td> <td> $brukernavn </td> <td> $etternavn </td> <td> $klassekode </td> </tr>");
+  }
+  print("</table>"); /*tabell slutt */
   ?>
