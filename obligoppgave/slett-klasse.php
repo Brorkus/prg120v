8ddc2f22c0893cb4b7fd707b555efcc757e3b2a6
Brorkus <filip.g.danielsen@gmail.com>
2025-10-24 15:55:23 +0200

    Update slett-klasse.php

--- obligoppgave/slett-klasse.php
+++ obligoppgave/slett-klasse.php
@@ -33,11 +33,12 @@ Slett klasse <select name="slettklasse" id="slettklasse">
               print ("Kan ikke slette klasse med studenter");
             }
           else
+            {
             include("dbconnect.php");  /* tilkobling til database */
-          $sqlSetning="SELECT * FROM klasse WHERE klassekode='$klassekode';";
-          $sqlResultat=mysqli_query($db,$sqlSetning) or die ("Kunne ikke hente data fra database");
-          $antallRader=mysqli_num_rows($sqlResultat); 
-            {	  
+            $sqlSetning="SELECT * FROM klasse WHERE klassekode='$klassekode';";
+            $sqlResultat=mysqli_query($db,$sqlSetning) or die ("Kunne ikke hente data fra database");
+            $antallRader=mysqli_num_rows($sqlResultat); 
+            	  
               $sqlSetning="DELETE FROM klasse WHERE klassekode='$klassekode';";
               mysqli_query($db,$sqlSetning) or die ("kunne ikke slette data i databasen");
                 /* SQL-setning sendt til database */
